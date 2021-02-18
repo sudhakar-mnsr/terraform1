692c8e36f107f1c32bd33f356fbfdcf17e51a2dc
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2021-02-18 16:50:41 +0530

    examples

--- terraform/running/1/s3.tf
+++ terraform/running/1/s3.tf
@@ -20,6 +20,13 @@ resource "aws_s3_bucket" "my_bucket" {
         days = 30
         storage_class = "STANDARD_IA"
       }
+      noncurrent_version_transition {
+        days = 60
+        storage_class = "GLACIER"
+      }
+      noncurrent_version_expiration {
+        days = 90
+      }
    }
 
    tags = {
