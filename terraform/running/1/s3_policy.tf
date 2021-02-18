2efe584a4da4b85a04989693e9abf03d42531b49
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2021-02-18 17:02:09 +0530

    examples

---
+++ terraform/running/1/s3_policy.tf
@@ -0,0 +1,64 @@
+provider "aws" {
+  region = var.region
+}
+
+data "aws_caller_identity" "my_account" {}
+
+resource "aws_s3_bucket" "my_bucket" {
+   bucket = "my-s3-bucket-aabbea"
+   region = var.region
+   acl = "public-read"
+
+   versioning {
+      enabled = true
+   }
+
+   lifecycle_rule {
+      prefix = "files/"
+      enabled = true
+
+      noncurrent_version_transition {
+        days = 30
+        storage_class = "STANDARD_IA"
+      }
+      noncurrent_version_transition {
+        days = 60
+        storage_class = "GLACIER"
+      }
+      noncurrent_version_expiration {
+        days = 90
+      }
+   }
+
+   tags = {
+      Type="LOG"
+      Tier="STANDARD"
+   }
+}
+
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+   bucket = aws_s3_bucket.my_bucket.id
+   policy = <<POLICY
+{
+   "version": "2012-10-1",
+   "Id": "MyBucketPolicy",
+   "statement": [
+      {
+         "Sid": "IPAllow",
+         "Effect": "Deny",
+         "Principal": "*"
+         "Action": "s3:*",
+         "Resource": "arn..."
+      }
+   ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_object" "readme_file" {
+   bucket = aws_s3_bucket.my_bucket.bucket
+   key = "files/readme.txt"
+
+   source = "readme.txt"
+   etag = filemd5("readme.txt")
+}
