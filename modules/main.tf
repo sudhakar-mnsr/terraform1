6fa9398e278dea9ad9d5b972ac1b417a19ac8885
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2021-02-20 15:23:02 +0530

    examples

--- modules/main.tf
+++ modules/main.tf
@@ -5,6 +5,6 @@ provider "aws" {
 module "vpc_module" {
    source = "./modules/vpc_networking"
    vpc_cidr_block = var.vpc_cidr_block
-   public_subnet_cidr = var.public_subnet_cidr
-   private_subnet_cidr = var.private_subnet_cidr
+   public_subnet_cidr = var.public_subnet_cidr_block
+   private_subnet_cidr = var.private_subnet_cidr_block
 }
