d7a504bca63ebeeca0db59382081415522b2b8d7
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2021-02-20 15:26:11 +0530

    examples

--- modules/main.tf
+++ modules/main.tf
@@ -5,6 +5,6 @@ provider "aws" {
 module "vpc_module" {
    source = "./modules/vpc_networking"
    vpc_cidr_block = var.vpc_cidr_block
-   public_subnet_cidr = var.public_subnet_cidr_block
-   private_subnet_cidr = var.private_subnet_cidr_block
+   public_subnet_cidr_block = var.public_subnet_cidr_block
+   private_subnet_cidr_block= var.private_subnet_cidr_block
 }
--- modules/modules/vpc_networking/main.tf
+++ modules/modules/vpc_networking/main.tf
@@ -7,12 +7,12 @@ resource "aws_vpc" "module_vpc" {
 }
 
 resource "aws_subnet" "public_subnet" {
-   cidr_block = var.public_subnet_cidr
+   cidr_block = var.public_subnet_cidr_block
    vpc_id = aws_vpc.module_vpc.id
 }
 
 resource "aws_subnet" "private_subnet" {
-   cidr_block = var.private_subnet_cidr
+   cidr_block = var.private_subnet_cidr_block
    vpc_id = aws_vpc.module_vpc.id
 }
 
