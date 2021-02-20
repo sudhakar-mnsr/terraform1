19cd33488e17b611f4af276d05a4c63de30a4431
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2021-02-20 14:28:31 +0530

    examples

--- modules/vpc_networking/variables.tf
+++ modules/vpc_networking/variables.tf
@@ -1,14 +1,11 @@
 variable "vpc_cidr_block" {
-   default = "10.0.0.0/24"
    description = "VPC CIDR Block"
 }
 
 variable "public_subnet_cidr" {
-   default = "10.0.1.0/24"
    description = "Public subnet CIDR"
 }
 
 variable "private_subnet_cidr" {
-   default = "10.0.2.0/24"
    description = "Private Subnet CIDR"
 }
