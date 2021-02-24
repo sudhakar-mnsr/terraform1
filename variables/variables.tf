4d92746d6b0749523bf53b26834eace8770c84e1
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2021-02-24 08:12:57 +0530

    examples

--- variables/variables.tf
+++ variables/variables.tf
@@ -72,6 +72,15 @@ variable "resource_tags" {
     project     = "project-alpha",
     environment = "dev"
   }
+  validation {
+    condition     = length(var.resource_tags["project"]) <= 16 && length(regexall("/[^a-zA-Z0-9-]/", var.resource_tags["project"])) == 0
+    error_message = "The project tag must be no more than 16 characters, and only contain letters, numbers, and hyphens."
+  }
+
+  validation {
+    condition     = length(var.resource_tags["environment"]) <= 8 && length(regexall("/[^a-zA-Z0-9-]/", var.resource_tags["environment"])) == 0
+    error_message = "The environment tag must be no more than 8 characters, and only contain letters, numbers, and hyphens."
+  }
 }
 
 variable "ec2_instance_type" {
